e8a36192a74ce044e34ca0824bbd79c3d33c0bbb
JuanitoAli <jcflorezv@unal.edu.co>
2017-10-07 11:47:35 -0500

    scaffold person :name :id :phone :email :age

---
+++ db/migrate/20171007164653_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :id
+      t.string :phone
+      t.string :email
+      t.string :age
+
+      t.timestamps
+    end
+  end
+end
